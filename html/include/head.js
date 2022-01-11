c5b7a4b60f90e709d085a8ad567baf96bdf1fdb2
Lim JeoungGyu <limjungkoro@gamil.com>
2022-01-11 18:16:22 +0900

    style 폴더명 styles 로 변경

--- html/include/head.js
+++ html/include/head.js
@@ -5,11 +5,11 @@ str += '<meta name="viewport" content="width=device-width, viewport-fit=cover" /
 str += '<meta name="theme-color" content="#6684EA" />';
 str += '<meta name="author" content="세종엔" />';
 str += '<title>세종엔</title>';
-str += '<link rel="stylesheet" href="../../public/style/swiper-bundle.min.css" />';
-str += '<link rel="stylesheet" href="../../public/style/gbSin.css"/>';
-str += '<link rel="stylesheet" href="../../public/style/gbInfo.css" />';
-str += '<link rel="stylesheet" href="../../public/style/gbMap.css" />';
-str += '<link rel="stylesheet" href="../../public/style/globals.css" />';
+str += '<link rel="stylesheet" href="../../public/styles/swiper-bundle.min.css" />';
+str += '<link rel="stylesheet" href="../../public/styles/gbSin.css"/>';
+str += '<link rel="stylesheet" href="../../public/styles/gbInfo.css" />';
+str += '<link rel="stylesheet" href="../../public/styles/gbMap.css" />';
+str += '<link rel="stylesheet" href="../../public/styles/globals.css" />';
 str += '<script src="../js//swiper-bundle.min.js"></script>';
 str += '<script src="../js/slider.js"></script>';
 str += '<script src="../js/ui.js"></script>';
