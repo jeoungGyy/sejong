bc0e4ab5973376c52df5c259a2c2ff40bdd9681d
Lim JeoungGyu <limjungkoro@gamil.com>
2022-01-26 11:54:37 +0900

    공통 헤더에 닫기 버튼 추가

--- html/include/header_sub.js
+++ html/include/header_sub.js
@@ -5,6 +5,7 @@ str += '  <div class="subHeader">';
 str += '    <button type="button" class="icoPrev"><i>이전 페이지</i></button>';
 str += '    <h2></h2>';
 str += '    <button type="button" class="icoPicture"><i>사진 목록보기</i></button>';
+str += '    <button type="button" class="icoClose"><i>닫기</i></button>';
 str += '  </div>';
 str += '</header>';
 
