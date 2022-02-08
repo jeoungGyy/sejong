8212a7fd59b0d1d05474d9c1d5e43554e08df5ad
Lim JeoungGyu <limjungkoro@gamil.com>
2022-02-08 14:32:40 +0900

    UI-SJN-41-001L 등록

--- html/js/slider.js
+++ html/js/slider.js
@@ -113,6 +113,11 @@ const slideOpt = {
     slidesPerView: 'auto',
 		spaceBetween: 8,
 	},
+	/* UI-SJN-41-001L */
+	satelliteSwiper: {
+    slidesPerView: 'auto',
+		spaceBetween: 8,
+	},
   merge : function(key){
     let merged = {...this.default, ...this[key]}
 		return merged;
