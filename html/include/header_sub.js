173c6998b55123841c9dff3bf845ecc785d22399
임정규 <limjungkoro@gmail.com>
2022-06-10 11:02:08 +0900

    정차장님 요청 수정사항

--- html/include/header_sub.js
+++ html/include/header_sub.js
@@ -3,9 +3,10 @@ var str = '';
 str += '<div class="headerWrap">';
 str += '  <div class="headerClear">';
 str += '   <header class="header">';
-str += '     <div class="subHeader">';
+str += '     <div class="subHeader offererAdd">';
 str += '       <button type="button" class="icoPrev"><i>이전 페이지</i></button>';
 str += '       <h2></h2>';
+str += '       <div class="offerer"><span>제공 : 한국환경공단</span></div>';
 str += '       <button type="button" class="icoPicture"><i>사진 목록보기</i></button>';
 str += '       <button type="button" class="icoClose"><i>닫기</i></button>';
 str += '     </div>';
@@ -14,3 +15,4 @@ str += '  </div>';
 str += '</div>';
 
 document.write(str);
+
