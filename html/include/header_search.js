235ee5c4cdc773633d90957a98c4e4cf65b28e2d
임정규 <limjungkoro@gmail.com>
2022-07-01 09:24:43 +0900

    인풋에 검색 버튼 추가

--- html/include/header_search.js
+++ html/include/header_search.js
@@ -7,6 +7,7 @@ str += '      <div class="subHeader">';
 str += '        <button type="button" class="icoPrev"><i>이전 페이지</i></button>';
 str += '        <div class="searchHeader">';
 str += '          <input type="text" title="장소, 버스 검색" placeholder="장소, 버스 검색" />';
+str += '          <button type="button" class="icoSearchBtn"><i class="split24">검색</i></button>';
 str += '        </div>';
 str += '        <div class="btnHeader">';
 str += '          <button type="button" class="icoMic"><i>마이크</i></button>';
